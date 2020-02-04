65103d160693a481ef310e0ed387a3004acf0d3e
Landon Morgan <morganlandon@gmail.com>
2020-02-04 13:08:53 -0600

    Initial Commit, practice exercise, chaining-methods completed

---
+++ chaining.js
@@ -0,0 +1,20 @@
+/* 
+
+Requirements
+Using one single line of JavaScript code, complete the following
+tasks on the array of integers below.
+
+- Sort the numbers in descending order (10, 9, 8, 7, etc).
+- Remove any integers greater than 19.
+- Multiply each remaining number by 1.5 and then subtract 1.
+- Then output (either in the DOM or the console) the sum of all the
+resulting numbers.
+
+*/
+
+const integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+
+const integers2 = integers.filter(x => x < 19).sort((a, b) => a - b).reverse().map(x => (x * 1.5) - 1).reduce((acc, crnt) => acc + crnt);
+
+console.log(integers2);
+
